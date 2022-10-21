e8da87280d290a95e86d6801eb5623c70282d029
Saiful Islam Sagor <54676388+saifulislam396@users.noreply.github.com>
2022-10-21 18:28:43 +0600

    Add files via upload

---
+++ mathe_method.js
@@ -0,0 +1,14 @@
+// mathe method start
+var a= Math.ceil(-1.60); /* Upper value Positiv down value negetive exmp: -1.50 output: 1*/
+var b= Math.floor(1.59) /* Down value  and value shuld be integer exmp: 1.50 output: 1*/
+var c=Math.round(-1.60) /* Upper value positvie negetive exmp: -1.50 ourput: 2 */
+var d=Math.trunc(1.59); /*value shuld be integer exmp: 1.50 output: 1*/
+var e=Math.max(66,76,23) /* Max value */
+var f=Math.min(66,76,23) /* Min value */
+var g=Math.sqrt(64) /* square root kore*/
+var h=Math.cbrt(27) /* cube root kore*/
+var i=Math.pow(2,10) /* power root kore*/
+var j=Math.floor(Math.random()*6)+ 1;
+var k=Math.abs(-5.25) /* only absolute value */
+var l=Math.PI;
+console.log(l);
---
+++ number_method.js
@@ -0,0 +1,35 @@
+// number method start
+// var a= '50';
+// var b= Number(a); /* number jodi stirn kore raka hoy tahole seta number e return kore */
+// console.log( 23 + b)
+
+// let num='22.55'
+// let c= parseInt(num) /* without desimal nmbr eta sudu integer nmbr return kore, eksate onk gula number value takle 1st er value nia take, word er sate nmbe dile sudu  nmber e return korbe */
+// console.log(c)
+
+// let num= 22.55;
+// let d= parseFloat(num); /* similar with parseInt but eta float value o return kore */
+// console.log(d)
+
+
+// js e is namer joto keyword ase ta sob dui ta value return kore
+// var num= '100';
+// console.log(Number.isFinite(num)) /* when number value is not then return false otherwise true */
+
+
+// var num= 24;
+// var f= Number.isInteger(num) /* when number is integer then return true otherwise false */
+// console.log(f)
+
+
+// var a=5.5000523;
+// var num=a.toFixed(2);/* ()er modde jotota nmbr dibo toto ta e . er pore desimal nmbeer nibe */
+// console.log(num)
+
+
+
+let aa= 24.5555;
+let num=aa.toPrecision(4);/* ()er modde jotota nmbr dibo toto ta e return korve */
+
+console.log(num)
+
